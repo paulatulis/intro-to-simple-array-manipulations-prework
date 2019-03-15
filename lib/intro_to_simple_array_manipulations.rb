e09c6ff769c5fc5d2c36816f82b9188d5d196f01
Paula Tulis <tulis.paula@gmail.com>
2019-03-15 19:32:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,14 @@
 def using_push(array,string)
   array.push(string)
 end
+
+def using_unshift(array,string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop 
+  return array
+end
+  
   
